17f4de022d9ea596d2b9428cc61ab0fa323b31ca
tuanlee88 <92803335+tuanlee88@users.noreply.github.com>
2021-12-01 19:06:50 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://hoangtuanhungthinh.blogspot.com/'>https://hoangtuanhungthinh.blogspot.com/</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://duannewgalaxynhatranghtl.blogspot.com/'>https://duannewgalaxynhatranghtl.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
